44d4cdb84be4c2e560fd1f3e9101d89b776b8fc6
Jared De Blander <jared@deblander.org>
2015-02-08 23:01:54 -0500

    latest updates to karma boterino

--- simba.js
+++ simba.js
@@ -5,6 +5,11 @@ var irc = require('node-twitch-irc'),
 
 // Create our commands object
 var commands = {
+    "#heidilisk": { 
+        "!greetings": "I greet you",
+        "!twitter": "Follow me on twitter: https://twitter.com/Heidilisku",
+        "!wellmet": "Well Met",
+    },
     "#karmaah": {
         "!about": "Karma's name is Jaime. She is 22 years young and lives in bean town (Boston, MA, USA)",
         "!ameno": "༼ つ ◕_◕ ༽つ AMENO ༼ つ ◕_◕ ༽つ",
@@ -12,19 +17,12 @@ var commands = {
         "!beast": "She's a beast, I call her Karma",
         "!bets": "Place your bets! If you are new here we bet on how many clicks it will take to break each of the 3 windows. Typically this is from 5-9 clicks. An example bet is 6/7/8.",
         "!blondie": "\\(⌒‿⌒✿)/",
-        // "!boys": "K A R M A B O Y S",
         "!crazybot": "Sadly Crazybot was DOA, I am his replacement! ",
-        // "!deck": "Karma's decklists can be found here: http://hskarma.blogspot.com/decks",
-        // "!decks": "Karma's decklists can be found here: http://hskarma.blogspot.com/decks",
-        // "!decklist": "Karma's decklists can be found here: http://hskarma.blogspot.com/decks",
-        // "!dance": "┏(-_-)┓┏(-_-)┛┗(-_- )┓┗(-_-)┛┏(-_-)┓ ┏(-_-)┛┗(-_- )┓┗(-_-)┛┏(-_-)┓┏(-_-)┛┗(-_- )┓┗(-_-)┛┏(-_-)┓┏(-_-)┛┗(-_- )┓┗(-_-)┛┏(-_-)┓",
+        "!emotes": "Subscriber emotes include karSimba (kar Simba), karDeal (kar Deal), karBM (kar BM) & karKappa (karK appa)",
         "!era": "Hunter God: http://www.twitch.tv/hsera",
         "!fedora": "http://gyazo.com/12c11cfc12cfa40f353fc9a9847669b1",
         "!gary": "Gary is a snail brought to you in four hundred and thirty-two lines of progressive scan definition.",
         "!gefft": "༼ つ ◕_◕ ༽つYOU GOT GEFFT ༼ つ ◕_◕ ༽つ",
-        // "!gifs": "http://hskarma.blogspot.com/p/gifs-mp4s.html",
-        // "!girls": "K A R M A G I R L S",
-        // "!grills": "K A R M A G I R L S",
         "!hacks": "Bitch, does it look like she's hacking?",
         "!huffer": "Huffer sent Karma this on snapchat: http://gyazo.com/2e240f2ea1d8273192451fab5d603820",
         "!jaraxxus": "http://i.gyazo.com/892a5842b5b5ab553a0b8f3732dfd67e.mp4",
@@ -39,7 +37,6 @@ var commands = {
         "!plugdj": "http://plug.dj/karmaah/",
         "!protest": "(◕‿◕✿) PEACEFUL PROTEST (◕‿◕✿)",
         "!ransom": "Hello my name is Simba. I have captured a woman known as Karma. If you ever want to see her again send catnip, playballs w/ bells, and treats w/ turkey to Boston, MA. Sincerely, your ruler.",
-        // "!rekt": "༼ つ ◕_◕ ༽つ REKT SLUT ༼ つ ◕_◕ ༽つ",
         "!sm4llz": "Checkout Sm4llz stream at http://www.twitch.tv/sm4llz",
         "!source": "The source code for KarmaBoterino can be found here: https://github.com/jared0x90/twitchbot/blob/master/simba.js",
         "!shades": "karDeal",
@@ -47,17 +44,13 @@ var commands = {
         "!subemotes": "Subscriber emotes include karSimba (kar Simba), karDeal (kar Deal), karBM (kar BM) & karKappa (karK appa)",
         "!tome": "Click for TOME access ~> http://kix.io/d4f <~",
         "!twittah": "Follow me on twitter for the latest stream news . www.twitter.com/KarmahTV",
-        "!website": "http://hskarma.blogspot.com/ Check out my site for decklists & the latest news!",
+        "!website": "http://hskarma.blogspot.com/ Check out my site for decklists & the latest news!"
     },
     "#jared0x90": {
         "!alive": "i am alive",
         "!areyouthere": "I LIVE"
-    },
-    "#hyp3d": {
-        "!boys": "H Y P 3 D B O Y S"
     }
 };
-
 
 // Create the IRC client
 var client = new irc.connect(
@@ -121,11 +114,6 @@ var client = new irc.connect(
             // repeatable methods
 
             // 600000 = 10 minutes
-            /*
-            setInterval(function () {
-                client.say("#karmaah" , "Click for TOME access ~> http://kix.io/d4f <~");
-            },  400000);
-*/
             setInterval(function () {
                 client.say("#karmaah" , "Twittah: http://twitter.com/karmahTV");
             },  500000);
